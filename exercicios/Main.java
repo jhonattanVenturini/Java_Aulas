f77f10d5dc08b3a6dd421ad360057504afc8c109
jhonattanVenturini <jhonattan.venturini@outlook.com>
2026-01-21 13:53:40 -0300

    feat: include aula01 e 02

--- exercicios/Main.java
+++ exercicios/Main.java
@@ -1,8 +1,15 @@
 package exercicios;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    var Person2 = new Person2();
-    
+    System.out.println();
+    System.out.println("-------------------------------");
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Digite o seu nome: ");
+    String name = sc.nextLine();
+    System.out.println("Olá, " + name + "!");
+    System.out.println("-------------------------------");
+    sc.close();
   }
 }
--- exercicios/Person2.java
+++ exercicios/Person2.java
@@ -3,7 +3,7 @@ package exercicios;
 public record Person2() {
   
   private static String name;
-
+  
 
 
 }
